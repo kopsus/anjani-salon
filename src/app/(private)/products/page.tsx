ec51db398fa7c2410752418b4b6ab8bfaa802d4f
kopsus <emonsetio01@gmail.com>
2025-05-05 21:33:35 +0700

    fix : dynamic page

--- src/app/(private)/products/page.tsx
+++ src/app/(private)/products/page.tsx
@@ -5,6 +5,7 @@ import { ColumnsProduct } from "@/components/table/product/Columns";
 import { DataTable } from "@/components/table/product/DataTable";
 import prisma from "@/lib/prisma";
 import React from "react";
+export const dynamic = "force-dynamic";
 
 const page = async () => {
   const products = await prisma.produk.findMany();
